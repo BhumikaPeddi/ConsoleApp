0d7f0b94c8b1670ad87514265277dd036f2e0c9b
Bhumika Peddi <BhumikaP@fixitytech.com>
2022-12-15 15:10:41 +0530

    ignore

---
+++ ConsoleApponGit/demo1/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApponGit.demo1
+{
+    internal class Class
+    {
+    }
+}
